4ee49f45f41766484cce1ca8f2184c0d2fb38ffc
uGriever <gui.leme657@gmail.com>
2024-05-24 15:26:23 -0300

    commit all

--- tipos-variaveis/src/variaveis.java
+++ tipos-variaveis/src/variaveis.java
@@ -1,5 +1,17 @@
 public class variaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short)numeroNormal;
+        
+       int numero=5;
+       numero=10;
+
+       final double PI= 3.14;
+       
+
+       System.out.print(PI);
     }
+
 }
